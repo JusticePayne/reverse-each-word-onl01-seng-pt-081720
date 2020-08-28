7fce0f9cec662e6bf3607d8fe6154432421b688a
Justice Payne <spikepayne@yahoo.com>
2020-08-28 19:10:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,10 @@ end
 reverse_each_word
 
 def reverse_each_word_with_each(string)
+  og_array = string.split(" ")
+  return_array = []
+  og_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
